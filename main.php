ce8760d07a03ef218834552ace4808d505a6c1b1
Piotr Ostrowski <piotrostr@google.com>
2023-11-07 17:39:04 +0100

    clean up

--- main.php
+++ main.php
@@ -4,27 +4,21 @@ require 'vendor/autoload.php';
 
 use Google\Client;
 
-// Set the path to your service account JSON key file
 $jsonKeyFilePath = 'creds.json';
 
-// Create a new Google Client
 $client = new Client();
 $client->setAuthConfig($jsonKeyFilePath);
 
-// Set the scopes for the access token
 $scopes = [
 	'https://www.googleapis.com/auth/cloud-platform',
-	// Add other scopes as needed
+	// the required Retail scopes
 ];
 
-// Set the client to use the application default credentials
 $client->useApplicationDefaultCredentials();
 $client->setScopes($scopes);
 
-// Fetch the access token
 $accessToken = $client->fetchAccessTokenWithAssertion();
 
-// Output the access token
 if (isset($accessToken['access_token'])) {
 	echo $accessToken['access_token'];
 } else {
